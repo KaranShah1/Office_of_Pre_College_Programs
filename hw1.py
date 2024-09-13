14598645d4d02986e2be19ccf63be3bc06dc669f
Karan Shah <65769054+KaranShah1@users.noreply.github.com>
2024-09-12 22:55:42 -0400

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -3,7 +3,7 @@ from openai import OpenAI, OpenAIError
 import fitz  # PyMuPDF for reading PDFs
 
 # Show title and description.
-st.title("Karan Shah📄 Document Question Answering")
+st.title("📄 Document Question Answering")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys)."
