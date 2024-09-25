8901ff8cf786cd1446cc53af6f78cee6e449456e
Karan Shah <65769054+KaranShah1@users.noreply.github.com>
2024-09-25 16:40:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ st.set_page_config(page_title="Multi-Page App", layout="wide")
 with st.sidebar:
     selected_page = option_menu(
         "Pre College Bot",
-        ["Test Bot", "Pre-College Bot"],
-        icons=['book', 'beaker'],
+        ["Test Bot", "Pre-College Bot", "Smart Pre-College Bot", "Staff Pre-College Bot],
+        icons=['beaker', 'beaker', 'beaker', 'beaker'],
         menu_icon="cast", 
         default_index=0,
     )
@@ -17,10 +17,20 @@ with st.sidebar:
 # Load the appropriate page based on the user's selection
 if selected_page == "Test Bot":
     st.title("SU Office of Pre-College Programs  ")
-    # Execute the hw1.py code
-    exec(open("hw1.py").read())  # This will run the content of hw1.py
+    # Execute the cps1.py code
+    exec(open("cps1.py").read())  # This will run the content of cps1.py
 
 elif selected_page == "Pre-College Bot":
     st.title("Syracuse University Office of Pre-College Programs")
     # Execute the hw2.py code
-    exec(open("hw2.py").read())  # This will run the content of hw2.py
+    exec(open("cps2.py").read())  # This will run the content of cps2.py
+
+elif selected_page == "Smart Pre-College Bot":
+    st.title("Syracuse University Office of Pre-College Programs")
+    # Execute the hw2.py code
+    exec(open("cps3.py").read())  # This will run the content of cps3.py
+
+elif selected_page == "Staff Pre College Bot":
+    st.title("Syracuse University Office of Pre-College Programs")
+    # Execute the cps4.py code
+    exec(open("cps4.py").read())  # This will run the content of cps4.py
