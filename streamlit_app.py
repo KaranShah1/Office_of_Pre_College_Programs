7b2968b826fa8855f15a8cef6165f1a4e0f03b99
Karan Shah <65769054+KaranShah1@users.noreply.github.com>
2024-10-06 18:31:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Multi-Page App", layout="wide")
 with st.sidebar:
     selected_page = option_menu(
         "Pre College Bot",
-        ["Test Bot", "Pre-College Bot", "Smart Pre-College Bot", "SRC Pre-College Bot"],
+        ["Test Bot", "Pre-College Bot", "Smart Pre-College Bot"],
         icons=['beaker', 'beaker', 'beaker', 'beaker'],
         menu_icon="cast", 
         default_index=0,
