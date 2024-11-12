e713dfc9f8dd75cd5475519e80d1bcbd971fdd3b
Karan Shah <65769054+KaranShah1@users.noreply.github.com>
2024-11-11 19:08:45 -0500

    Update cps5.py

--- cps5.py
+++ cps5.py
@@ -22,7 +22,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            .st-emotion-cache-30do4w.e3g6aar1 {visibility: hidden;}
+            .st-emotion-cache-30do4w.e3g6aar1 {display: none;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
